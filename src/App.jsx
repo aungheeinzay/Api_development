a88d9262d5eccf92abc66d6296e82a5339379432
AungHeeinZay <ahz007aunghz@gmail.com>
2025-06-28 01:36:04 +0630

    starter kid

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import {createBrowserRouter,BrowserRouter} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='text-red-600'>hello vite</div>
+  );
+}
+
+export default App;
